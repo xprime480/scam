be479d3311513f8099862754f25242336f57efdf
Michael Davis <mi.davis@sap.com>
2018-09-25 13:08:04 -0700

    More complex WorkerThread in queue

--- unit/main.cpp
+++ unit/main.cpp
@@ -13,14 +13,46 @@ class OneShotWorker : public Worker
 public:
     void run() override
     {
-	cout << "OneShotWorker here\n";
+        cout << "OneShotWorker here\n";
     }
+};
+
+class Countdown : public Worker
+{
+public:
+    Countdown(size_t n, WorkQueue & queue)
+        : n(n)
+        , queue(queue)
+    {
+    }
+
+    void run() override
+    {
+        if ( 0 == n ) {
+            cout << "go!!\n";
+        }
+        else {
+            cout << n << "... ";
+
+            WorkerHandle next = std::make_shared<Countdown>(n-1, queue);
+            queue.put(next);
+        }
+    }
+
+private:
+    size_t n;
+    WorkQueue & queue;
 };
 
 int main(int argc, char ** argv)
 {
     WorkQueue queue;
+
+    WorkerHandle count = std::make_shared<Countdown>(3, queue);
+    queue.put(count);
+
     WorkerHandle start = std::make_shared<OneShotWorker>();
     queue.put(start);
+
     Trampoline(queue);
 }
