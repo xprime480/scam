a296d079856d9fc66f9f54958a2ef78190198726
Michael Davis <mi.davis@sap.com>
2018-11-07 15:19:35 -0800

    Remove excess functions in Binder class
        They were added in previous commit but not used.

--- include/Binder.hpp
+++ include/Binder.hpp
@@ -13,14 +13,11 @@ namespace scam
         Binder(Env capture);
 
         Env bind(ScamExpr * formals, ScamExpr * actuals) const;
-        Env prebind(ScamExpr * formals) const;
-        void rebind(Env env, ScamExpr * formals, ScamExpr * actuals) const;
 
     private:
         Env capture;
 
         void bindOne(Env env, ScamExpr * syms, ScamExpr * vals) const;
-        void rebindOne(Env env, ScamExpr * formals, ScamExpr * actuals) const;
     };
 }
 
