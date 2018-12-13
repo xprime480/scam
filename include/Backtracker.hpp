aebf0390a6f1555f88a45876584395c6317a63f2
Michael Davis <mi.davis@sap.com>
2018-12-13 10:49:20 -0800

    Enhance Backtracker class
        Add 'parent' member to facilitate chaining
        getParent() accessor
        runParent(cont) to execute next level of backtracking
        safeID(handle) pointer safe way to get id, '<null>' if null handle
        dumpStack(handle) print parent chain of backtrackers

--- include/Backtracker.hpp
+++ include/Backtracker.hpp
@@ -8,21 +8,32 @@
 
 namespace scam
 {
+    class Backtracker;
+    using BacktrackHandle = std::shared_ptr<Backtracker>;
+
     class Backtracker
     {
     public:
-        Backtracker(char const * id);
+        Backtracker(char const * id, BacktrackHandle parent);
         virtual ~Backtracker();
+
+        static void dumpStack(BacktrackHandle bt);
+        static std::string safeID(BacktrackHandle bt);
 
         virtual void run(ContHandle cont);
         std::string id() const;
 
+    protected:
+        void runParent(ContHandle) const;
+        BacktrackHandle getParent() const;
+
     private:
         std::string const name;
+        BacktrackHandle parent;
+
         static std::string makeName(char const * id);
     };
 
-    using BacktrackHandle = std::shared_ptr<Backtracker>;
 }
 
 #endif
--- src/Backtracker.cpp
+++ src/Backtracker.cpp
@@ -1,6 +1,7 @@
 
 #include "Backtracker.hpp"
 
+#include "expr/ExpressionFactory.hpp"
 #include "expr/ScamExpr.hpp"
 
 #include <iostream>
@@ -14,8 +15,9 @@ namespace
     static unsigned counter { 0 };
 }
 
-Backtracker::Backtracker(char const * id)
+Backtracker::Backtracker(char const * id, BacktrackHandle parent)
     : name(makeName(id))
+    , parent(parent)
 {
     //    cerr << "Creating backtracker " << name << "\n";
 }
@@ -25,6 +27,26 @@ Backtracker::~Backtracker()
     //    cerr << "Deleting backtracker " << name << "\n";
 };
 
+void Backtracker::dumpStack(BacktrackHandle bt)
+{
+    if ( nullptr == bt.get() ) {
+        cerr << "<no backtracker stack>\n";
+        return;
+    }
+
+    for ( size_t n = 1 ; nullptr != bt.get() ; ++n, bt = bt->getParent() ) {
+        cerr << "[" << n << "]\t" << bt->id() << "\n";
+    }
+}
+
+string Backtracker::safeID(BacktrackHandle bt)
+{
+    if ( nullptr == bt.get() ) {
+        return "<null backtracker>";
+    }
+    return bt->id();
+}
+
 void Backtracker::run(ContHandle cont)
 {
     //    cerr << "Executing backtracker " << name << "\n";
@@ -33,6 +55,23 @@ void Backtracker::run(ContHandle cont)
 string Backtracker::id() const
 {
     return name;
+}
+
+BacktrackHandle Backtracker::getParent() const
+{
+    return parent;
+}
+
+void Backtracker::runParent(ContHandle cont) const
+{
+    if ( nullptr == parent.get() ) {
+        static const ExprHandle nomore =
+            ExpressionFactory::makeError("No more choices");
+        cont->run(nomore.get());
+    }
+    else {
+        parent->run(cont);
+    }
 }
 
 string Backtracker::makeName(char const * id)
