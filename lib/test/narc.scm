25ba9be1c55278cbf52a8747e0eaaf0d0e644d62
Mike Davis <xprime480@gmail.com>
2019-06-21 13:13:55 -0700

    Add beginning of test framework
        lib/test/narc.scm
    
        Add 'test' target to Makefile in scam directory to discover and run
            all scheme files in specified directory as tests.

---
+++ lib/test/narc.scm
@@ -0,0 +1,25 @@
+
+(define **narc-test-name** "")
+(define **narc-test-count** 0)
+(define **narc-test-pass** 0)
+
+(define narc-label
+  (lambda (name)
+    (assign! **narc-test-name** name)))
+
+(define narc-expect
+  (macro forms))
+
+(define narc-catch
+  (macro forms))
+
+(define narc-report
+  (lambda ()
+    (let ((pass (= **narc-test-count** **narc-test-pass**)))
+      (display (if pass "[PASS]" "[FAIL]"))
+      (display " ")
+      (display **narc-test-name**)
+      (newline)
+      pass)))
+
+0
