60edb3b16c42274545b2b45877e7c88012be50ad
Mike Davis <xprime480@gmail.com>
2019-07-18 09:48:13 -0700

    Extend Syntax evaluation to substitute symbols.
        Now expansions with side effects will effect the correct value.

--- src/form/SyntaxUtils.cpp
+++ src/form/SyntaxUtils.cpp
@@ -8,9 +8,19 @@
 #include "util/LambdaDef.hpp"
 #include "util/Parameter.hpp"
 
-#include "util/GlobalId.hpp"
-#include "util/DebugTrace.hpp"
-#include "expr/ValueWriter.hpp"
+#include <map>
+
+using namespace scam;
+using namespace std;
+
+namespace
+{
+    using FormalsToActuals = map<string, ScamValue>;
+
+    ScamValue substitute(const LambdaDef & lambda, ScamValue actuals);
+    ScamValue substituteForm(ScamValue form, const FormalsToActuals & f2a);
+    ScamValue conformalError(ScamValue formals, ScamValue actuals);
+}
 
 bool scam::installSyntax(Env * env,
                          ScamEngine * engine,
@@ -19,33 +29,22 @@ bool scam::installSyntax(Env * env,
 {
     const char * name = symbol->stringValue().c_str();
 
-    GlobalId id;
-    //ScamTraceScope _;
-    scamTrace(id, __FILE__, __LINE__, __FUNCTION__,
-              writeValue(symbol), writeValue(rules));
-
     SymbolParameter p0;
     ListParameter   p1;
     ObjectParameter pObj;
     ListOfParameter p2(pObj);
 
     if ( argsToParms(rules, engine, name, p0, p1, p2) ) {
-        scamTrace(id, __FILE__, __LINE__, __FUNCTION__, writeValue(p2.value));
-
         ScamValue def = p2.value;
         ListParameter    sp0;
         ObjectParameter  spObj;
         CountedParameter sp1(spObj);
         if ( argsToParms(def, engine, name, sp0, sp1) ) {
-            scamTrace(id, __FILE__, __LINE__, __FUNCTION__,
-                      writeValue(sp0.value), writeValue(sp1.value));
-
             LambdaDef lambda;
             lambda.formals = getCdr(sp0.value);
             lambda.forms   = sp1.value;
 
             ScamValue value = makeSyntax(name, lambda);
-            scamTrace(id, __FILE__, __LINE__, __FUNCTION__, writeValue(value));
             env->put(symbol, value);
             return true;
         }
@@ -60,16 +59,78 @@ void scam::applySyntax(ScamValue value,
                        Env * env,
                        ScamEngine * engine)
 {
-    GlobalId id;
-    ScamTraceScope _;
-    scamTrace(id, __FILE__, __LINE__, __FUNCTION__,
-              writeValue(value), writeValue(args));
-
     const LambdaDef & def = value->syntaxDef();
-    scamTrace(id, __FILE__, __LINE__, __FUNCTION__, writeValue(def.forms));
+    LambdaDef expanded;
+    expanded.valid = true;
+    expanded.forms = substitute(def, args);
 
-    // simplest thing that makes the test pass!
+    ScamValue closure = makeClosure(expanded, env, false);
+    apply(closure, makeNull(), cont, env, engine);
+}
 
-    ScamValue closure = makeClosure(def, env, false);
-    apply(closure, args, cont, env, engine);
+namespace
+{
+    ScamValue substitute(const LambdaDef & lambda, ScamValue actuals)
+    {
+        FormalsToActuals f2a;
+        ScamValue f = lambda.formals;
+        ScamValue a = actuals;
+
+        while ( ! isNull(f) ) {
+            if ( isNull(a) ) {
+                return conformalError(lambda.formals, actuals);
+            }
+
+            const string & key = getCar(f)->stringValue();
+            ScamValue      val = getCar(a);
+            f2a[key] = val;
+
+            f = getCdr(f);
+            a = getCdr(a);
+        }
+
+        if ( ! isNull(a) ) {
+            return conformalError(lambda.formals, actuals);
+        }
+
+        ScamValue rv = substituteForm(lambda.forms, f2a);
+        return rv;
+    }
+
+    ScamValue substituteForm(ScamValue form, const FormalsToActuals & f2a)
+    {
+        if ( f2a.empty() ) {
+            return form;
+        }
+
+        ScamValue rv = form;
+
+        if ( isList(form) ) {
+            vector<ScamValue> newForms;
+            while ( ! isNull(form) ) {
+                ScamValue head = getCar(form);
+                form           = getCdr(form);
+                ScamValue newValue = substituteForm(head, f2a);
+                newForms.push_back(newValue);
+            }
+            rv = makeList(newForms);
+        }
+
+        else if ( isSymbol(form) ) {
+            const string & name = form->stringValue();
+            const auto iter = f2a.find(name);
+            if ( f2a.end() != iter ) {
+                rv = iter->second;
+            }
+        }
+
+        return rv;
+    }
+
+    ScamValue conformalError(ScamValue formals, ScamValue actuals)
+    {
+        static const char * msg
+        { "formals are not conformal with actuals (%{0}) (%{1})" };
+        return makeError(msg, formals, actuals);
+    }
 }
